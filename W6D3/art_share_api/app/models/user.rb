a4395007f429c031c5440bda518759eb2092570c
App Academy Student <student@appacademy.io>
2019-09-25 18:17:24 -0700

    W6D3 cw

---
+++ W6D3/art_share_api/app/models/user.rb
@@ -0,0 +1,13 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  username   :string           not null
+#
+
+class User < ApplicationRecord
+  validates :username, presence: true
+end
